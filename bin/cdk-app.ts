675ba29a8c6f875bc0f12723732dcac8ffa7788b
Jonathan Oudshoorn <jonathan.oudshoorn@ns.nl>
2025-07-01 11:47:46 +0200

    cloud front distribution

--- bin/cdk-app.ts
+++ bin/cdk-app.ts
@@ -4,9 +4,17 @@ import { App } from 'aws-cdk-lib';
 import { CdkFeAppStack } from '../lib/cdk-fe-app-stack';
 
 const app = new App();
+const stackEnv = app.node.tryGetContext("dev");
+
 new CdkAppStack(app, 'CdkAppStack', {
 });
 
 new CdkFeAppStack(app, 'CdkFrontendAppStack', {
-
+    domainName: stackEnv.domainName,
+    siteSubDomain: stackEnv.siteSubDomain,
+    certificateArn: stackEnv.certificateArn,
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEFAULT_REGION
+    }
 });
--- lib/cdk-fe-app-stack.ts
+++ lib/cdk-fe-app-stack.ts
@@ -1,52 +1,95 @@
-import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import {
+  CfnOutput, Duration,
+  RemovalPolicy,
+  Stack,
+  StackProps
+} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { Bucket, CfnBucketPolicy } from 'aws-cdk-lib/aws-s3';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import {
+  AccessLevel,
+  Distribution,
+  OriginAccessIdentity,
+  SecurityPolicyProtocol,
+  ViewerProtocolPolicy
+} from 'aws-cdk-lib/aws-cloudfront';
+import { ARecord, HostedZone, RecordTarget } from 'aws-cdk-lib/aws-route53';
+import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
+import { CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets';
+import { S3BucketOrigin } from 'aws-cdk-lib/aws-cloudfront-origins';
+
+interface StaticSiteStackProps extends StackProps{
+  domainName: string;
+  siteSubDomain: string;
+  certificateArn: string;
+}
 
 export class CdkFeAppStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props: StaticSiteStackProps) {
     super(scope, id, props);
 
-    const bucketName = 'cdkapp-site-bucket';
+    const { domainName, siteSubDomain } = props;
+    const siteDomain = `${siteSubDomain}.${domainName}`;
 
     const siteBucket = new Bucket(this, 'SiteBucket', {
-      bucketName: bucketName,
-      websiteIndexDocument: 'index.html',
-      websiteErrorDocument: 'error.html',
-      publicReadAccess: true,
-      blockPublicAccess: {
-        blockPublicAcls: false,
-        blockPublicPolicy: false,
-        ignorePublicAcls: false,
-        restrictPublicBuckets: false,
-      },
+      bucketName: siteDomain,
+      publicReadAccess: false,
       removalPolicy: RemovalPolicy.DESTROY,
       autoDeleteObjects: true
     });
 
-    new BucketDeployment(this, 'DeployWithValidation', {
-      sources: [Source.asset('./src/frontend-app')],
-      destinationBucket: siteBucket
+    const zone = HostedZone.fromLookup(this, 'Zone', {domainName});
+
+    const originAccessIdentity = new OriginAccessIdentity(this, 'OAI');
+    siteBucket.grantRead(originAccessIdentity);
+
+    // certificate was created manually since it has to be in us-east-1.
+    const certificateArn = props.certificateArn;
+
+    const s3Origin = S3BucketOrigin.withOriginAccessControl(siteBucket, {
+      originAccessLevels: [AccessLevel.READ],
+    });// Ruben
+
+    const distribution = new Distribution(this, 'SiteDistribution', {
+      defaultBehavior: {
+        origin: s3Origin,
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      defaultRootObject: 'index.html',
+      domainNames: [siteDomain],
+      certificate: Certificate.fromCertificateArn(this, 'Cert', certificateArn),
+      minimumProtocolVersion: SecurityPolicyProtocol.TLS_V1_2_2021,
+      errorResponses: [
+        {
+          httpStatus: 403,
+          responseHttpStatus: 200,
+          responsePagePath: '/error.html',
+          ttl: Duration.minutes(5),
+        },
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/error.html',
+          ttl: Duration.minutes(5),
+        },
+      ],
     });
 
-    const bucketPolicy = new CfnBucketPolicy(this, "BucketPolicy", {
-      bucket: bucketName,
-      policyDocument: {
-        Statement: [
-          {
-            Action: 's3:getObject',
-            Effect: 'Allow',
-            Principal: {
-              AWS: '*',
-            },
-            Resource: [
-              siteBucket.bucketArn,
-              `${siteBucket.bucketArn}/*`
-            ],
-          },
-        ],
-        Version: '2012-10-17',
-      },
+    new ARecord(this, 'SiteAliasRecord', {
+      recordName: siteDomain,
+      target: RecordTarget.fromAlias(new CloudFrontTarget(distribution)),
+      zone
     });
+
+    new BucketDeployment(this, 'DeployWithValidation', {
+      sources: [Source.asset('./src/frontend-app')],
+      destinationBucket: siteBucket,
+      distribution,
+      distributionPaths: ['/*']
+    });
+
+    new CfnOutput(this, 'SiteUrl', {value: `https://${siteDomain}`});
+
   }
 }
