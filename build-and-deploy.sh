69fe2a7c3722d90c4e4fe7bd031f0747415a8acc
Jonathan Oudshoorn <jonathan.oudshoorn@ns.nl>
2025-08-14 14:16:47 +0200

    updated to pipelines v2

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-cd cdk
-cdk deploy
+#refresh angular build and deploy (should done first so that the correct index.html will be rebuild)
+cd angular-app
+npm install
+npm run build
 
+# refresh CDK build and deploy
+cd ../cdk
+rm -rf ./build
+npm install
+npm run build
+cdk deploy --all
--- cdk/lib/pipeline-stack.ts
+++ cdk/lib/pipeline-stack.ts
@@ -2,6 +2,7 @@ import { Construct } from 'constructs';
 import { pipelines, Stack, StackProps } from 'aws-cdk-lib';
 import { AppStage } from './app-stage';
 import { BuildSpec } from 'aws-cdk-lib/aws-codebuild';
+import { Pipeline, PipelineType } from 'aws-cdk-lib/aws-codepipeline';
 
 export class PipelineStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
@@ -9,46 +10,49 @@ export class PipelineStack extends Stack {
 
         const connectionArn = 'arn:aws:codeconnections:eu-west-1:836024728316:connection/ec297c1f-b4f0-4d20-ad56-48c0109cba28';
 
+        const v2 = new Pipeline(this, 'UnderlyingV2', {
+            pipelineType: PipelineType.V2,
+            pipelineName: 'VectorIt-website-pipeline',
+        });
+
         const pipeline = new pipelines.CodePipeline(this, 'Pipeline', {
-            pipelineName: 'VectorIT-website-Pipeline',
+            codePipeline: v2,
             selfMutation: true,
 
             synth: new pipelines.ShellStep('Synth', {
                 input: pipelines.CodePipelineSource.connection(
                     'TheVectoRR/cdk-angular-cloudfront-demo',
                     'main',
-                    { connectionArn, },
-
+                    { connectionArn },
                 ),
                 commands: [
-                    // refresh angular build and deploy (should be done first)
+                    // Angular build first
                     'cd angular-app',
                     'npm ci',
                     'npm run build',
 
-                    // refresh CDK build and synth
+                    // CDK build + synth
                     'cd ../cdk',
                     'rm -rf ./build',
                     'npm ci',
                     'npm run build',
-                    'npx cdk synth'
+                    'npx cdk synth',
                 ],
                 primaryOutputDirectory: 'cdk/cdk.out',
             }),
+
             synthCodeBuildDefaults: {
                 partialBuildSpec: BuildSpec.fromObject({
                     phases: {
                         install: {
-                            "runtime-versions": {
-                                nodejs: "22"
-                            }
-                        }
-                    }
-                })
-            }
+                            'runtime-versions': { nodejs: '22' },
+                        },
+                    },
+                }),
+            },
         });
 
-        pipeline.addStage(new AppStage(this, 'Dev', {
+        pipeline.addStage(new AppStage(this, 'DeployStacks', {
             env: { account: this.account, region: 'eu-west-1' },
             contextEnv: 'dev',
         }));
