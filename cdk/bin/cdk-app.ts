9861d5cabd3f19cd2d90a261f6dff12e810e30a8
Jonathan Oudshoorn <jonathan.oudshoorn@ns.nl>
2025-08-18 14:00:12 +0200

    reverted pipeline stack - prefer not to use it at this time

--- cdk/bin/cdk-app.ts
+++ cdk/bin/cdk-app.ts
@@ -1,8 +1,20 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { PipelineStack } from '../lib/pipeline-stack';
+import { CdkAppStack } from '../lib/cdk-app-stack';
+import { App } from 'aws-cdk-lib';
+import { CdkFeAppStack } from '../lib/cdk-fe-app-stack';
 
-const app = new cdk.App();
-new PipelineStack(app, 'PipelineStack', {
-    env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const app = new App();
+const stackEnv = app.node.tryGetContext("dev");
+
+new CdkAppStack(app, 'CdkAppStack', {
+});
+
+new CdkFeAppStack(app, 'CdkFrontendAppStack', {
+    domainName: stackEnv.domainName,
+    siteSubDomain: stackEnv.siteSubDomain,
+    certificateArn: stackEnv.certificateArn,
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEFAULT_REGION
+    }
 });
